34e68fcfc7753281c4907066e17e0aea24dae9a5
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-03-14 10:30:13 -0700

    fix up scripts

--- autoupload-fetch/autoupload/create-url.js
+++ autoupload-fetch/autoupload/create-url.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const cloudinary = require('cloudinary').v2
 const open = require('open')
 
-// create an auto-upload image tag with url with transformations
-// add the tag created here to the index.html
+// upload by referencing folder mapped to auto upload remote path
 const url = cloudinary.url(cloudinary.url('remote-media/images/pineapple.jpg'))
 console.log(url)
 open(url)
--- autoupload-fetch/fetch/fetch-tranform.js
+++ autoupload-fetch/fetch/fetch-tranform.js
@@ -7,14 +7,11 @@ const url = cloudinary.url(`${process.env.ASSET_SOURCE_BASE}/assets/images/orang
     type: 'fetch',
     width: 400,
     height: 400,
-    crop: 'fill',
-    gravity: 'auto',
-    radius: 'max',
+    crop: 'limit',
+    radius: '30',
     effect: 'sharpen',
     fetch_format: 'auto'
   }
 )
 console.log(url)
 open(url)
-
-// http://res.cloudinary.com/picturecloud7/image/fetch/c_fill,e_sharpen,f_auto,g_auto,h_400,r_max,w_400/`${process.env.ASSET_SOURCE_BASE}/assets/images/oranges.jpg
