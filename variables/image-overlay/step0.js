f950d663f5116e859407b7be8b9c69d03913f18a
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-03-20 15:48:32 -0700

    add steps for overylay and variables

---
+++ variables/image-overlay/step0.js
@@ -0,0 +1,9 @@
+// upload a small video such as a customer might upload from an iphone
+require('dotenv').config()
+
+const cloudinary = require('cloudinary').v2
+const open = require('open')
+
+const url = cloudinary.url('baseball-cap', {})
+
+open(url)
---
+++ variables/image-overlay/step4.js
@@ -0,0 +1,38 @@
+// upload a small video such as a customer might upload from an iphone
+require('dotenv').config()
+
+const cloudinary = require('cloudinary').v2
+const open = require('open')
+
+const url = cloudinary.url('baseball-cap', {
+  transformation: [
+    {
+      variables: [
+        ['$w', 500],
+        ['$correctx', '0.04'],
+        ['$correcty', '0.1'],
+        ['$logoscalar', '0.4'],
+        ['$color', '!ffffff!']
+      ]
+    },
+    {
+      effect: 'replace_color:$color:30:111111'
+    },
+    { width: '$w', crop: 'scale' },
+    {
+      overlay: 'logo-big',
+      crop: 'fit',
+      gravity: 'north',
+      width: '$width * $logoscalar',
+      x: '$width * $correctx',
+      y: '$width * 0.1'
+    },
+    {
+      dpr: '2.0',
+      fetch_format: 'auto',
+      quality: 'auto'
+    }
+  ]
+})
+console.log(url)
+open(url)
