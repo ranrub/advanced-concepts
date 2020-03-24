5bc440e72c4c55c9760ded170b5fbfe2c1204b93
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-03-24 07:59:52 -0700

    make moderated private

--- user-upload-workflow/quality-moderation/quality-moderate.js
+++ user-upload-workflow/quality-moderation/quality-moderate.js
@@ -1,29 +1,15 @@
 require('dotenv').config()
-// require('dotenv').config({ path: './.env.backup' })
-
 const cloudinary = require('cloudinary').v2
 
 cloudinary.uploader
-  .upload('./assets/images/blurry-image.jpg', {
-    public_id: 'blurry-image',
-    quality_analysis: true
+  .upload('./assets/images/image-from-tv.jpg', {
+    type: 'private',
+    public_id: 'image-from-tv-1',
+    quality_analysis: true,
+    moderation: 'manual',
+    invalidate: true
   })
   .then(uploadResult => {
     console.log(uploadResult)
-    // try .8, .9
-    if (uploadResult.quality_analysis.focus < 0.7) {
-      cloudinary.uploader
-        .explicit(uploadResult.public_id, {
-          type: 'upload',
-          moderation: 'manual',
-          notification_url:
-            'https://webhook.site/17a3d46c-5d18-46b0-ab6c-94b12d7f645c'
-        })
-        .then(result => console.log(result))
-        .catch(error => console.log(error))
-    }
   })
-
   .catch(error => console.error(error))
-
-// 'https://images.pexels.com/photos/1242348/pexels-photo-1242348.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
