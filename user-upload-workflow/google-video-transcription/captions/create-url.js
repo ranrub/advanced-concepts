9a0e10443d3f11ae24c1b3abac6bf986d2acec3a
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-03-27 13:58:30 -0700

    secure url

--- user-upload-workflow/google-video-transcription/captions/create-url.js
+++ user-upload-workflow/google-video-transcription/captions/create-url.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const cloudinary = require('cloudinary').v2
 
-const url = cloudinary.url('ul-video.vtt', { resource_type: 'raw' })
+const url = cloudinary.url('ul-video.vtt', {
+  resource_type: 'raw',
+  secure_url: true
+})
 console.log(url)
 open(url)
