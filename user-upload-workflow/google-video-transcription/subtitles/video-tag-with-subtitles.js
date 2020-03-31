1ecc034b797658791e18f4e5d831baaee421ba4d
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-03-31 08:06:48 -0700

    update video workflow

--- user-upload-workflow/google-video-transcription/subtitles/video-tag-with-subtitles.js
+++ user-upload-workflow/google-video-transcription/subtitles/video-tag-with-subtitles.js
@@ -5,6 +5,7 @@ const video = cloudinary.video('ul-video', {
   overlay: {
     public_id: 'subtitles:ul-video.srt'
   },
+  controls: true,
   background: 'black',
   color: 'yellow',
   gravity: 'north'
