88dae13bc2cb1195c0a0f4fa3be63b493f7ec99d
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-04-04 18:40:24 -0700

    use cloudinary-training;cleanup

--- user-upload-workflow/remote-functions/README.md
+++ user-upload-workflow/remote-functions/README.md
@@ -8,3 +8,5 @@ curl --location --request POST 'https://secure-caverns-90265.herokuapp.com/api/f
 --header 'Content-Type: multipart/form-data; boundary=--------------------------279813939285989578662257' \
 --form 'name=@/Users/rebeccapeltz/training-images/cloudinary-training.jpg'
 ```
+## Custom Function: remote function in transformation
+https://res.cloudinary.com/cloudinary-training/image/upload/s--ZSceRCol--/fn_remote:aHR0cHM6Ly9zZWN1cmUtY2F2ZXJucy05MDI2NS5oZXJva3VhcHAuY29tL2FwaS9maWxl/shell
--- user-upload-workflow/remote-functions/remote-custom-url.js
+++ user-upload-workflow/remote-functions/remote-custom-url.js
@@ -5,12 +5,15 @@ const open = require('open')
 
 const url = cloudinary.url('shell', {
   sign_url: true,
-  custom_function: {
-    function_type: 'remote',
-    source: 'https://secure-caverns-90265.herokuapp.com/api/file'
-  }
+  transformation: [
+    {
+      custom_function: {
+        function_type: 'remote',
+        source: 'https://secure-caverns-90265.herokuapp.com/api/file'
+      }
+    },
+    { border: '5px_solid_coral' }
+  ]
 })
 console.log(url)
 open(url)
-
-// https://res.cloudinary.com/pictures77/image/upload/s--PMUhGYJY--/fn_remote:aHR0cHM6Ly9zZWN1cmUtY2F2ZXJucy05MDI2NS5oZXJva3VhcHAuY29tL2FwaS9maWxl/shell
