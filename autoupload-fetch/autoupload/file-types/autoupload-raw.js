bcfe9188d131f24891d9e02764f5a7033190e4d2
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-04-06 16:36:14 -0700

    remove bopen

--- autoupload-fetch/autoupload/file-types/autoupload-raw.js
+++ autoupload-fetch/autoupload/file-types/autoupload-raw.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const cloudinary = require('cloudinary').v2
-const bopen = require('bopen')
+const open = require('open')
 
 const url = cloudinary.url('remote-media/raw/data.json', {
   resource_type: 'raw',
   secure: true
 })
 console.log(url)
-bopen(url, { browser: 'chrome', incognito: true }) // bypass browser cache
+open(url, { app: ['google chrome', '--incognito'] }) // bypass browser cache
