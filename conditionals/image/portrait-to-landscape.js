2dccdc118b3b7098223d76b4ca81b96d61c6e427
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-04-09 11:47:44 -0700

    update conditional

--- conditionals/image/portrait-to-landscape.js
+++ conditionals/image/portrait-to-landscape.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const cloudinary = require('cloudinary').v2
 const open = require('open')
-
 cloudinary.uploader
   .upload('./assets/images/woman-standing.jpg')
   .then(result => {
@@ -9,7 +8,14 @@ cloudinary.uploader
       transformation: [
         { variables: [['$wide', '300']] },
         { if: 'ar_lt_1.0' },
-        { aspect_ratio: '1.5', width: '$wide', crop: 'fill', gravity: 'auto' },
+        {
+          aspect_ratio: '1.5',
+          width: '$wide',
+          crop: 'fill',
+          gravity: 'face',
+          effect: 'grayscale'
+        },
+        { effect: 'tint:50:green:yellow' },
         { if: 'end' }
       ]
     })
