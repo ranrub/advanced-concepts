087e91ac9c3b6536605d2154b2b34424599eff53
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-04-09 08:35:22 -0700

    update to pineapple;add create preset code

--- autoupload-fetch/autoupload/create-upload-url.js
+++ autoupload-fetch/autoupload/create-upload-url.js
@@ -4,6 +4,6 @@ const open = require('open')
 
 // upload by referencing folder mapped to auto upload remote path
 // look at network caching
-const url = cloudinary.url('remote-media/images/dolphin.jpg')
+const url = cloudinary.url('remote-media/images/pineapple.jpg')
 console.log(url)
 open(url, { browser: 'chrome', incognito: true }) // bypass browser cache
