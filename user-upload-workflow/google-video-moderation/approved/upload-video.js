1c9ad867c329406ef92555041ae29edeef1ece9b
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-04-08 17:10:07 -0700

    shorten video

--- user-upload-workflow/google-video-moderation/approved/upload-video.js
+++ user-upload-workflow/google-video-moderation/approved/upload-video.js
@@ -9,6 +9,7 @@ cloudinary.uploader
     public_id: 'elephants',
     access_control: [{ access_type: 'token' }],
     moderation: 'google_video_moderation:possible',
+    transformation: [{ duration: 5 }],
     notification_url:
       'https://webhook.site/17a3d46c-5d18-46b0-ab6c-94b12d7f645c'
   })
@@ -17,4 +18,3 @@ cloudinary.uploader
     console.log(result.moderation.repsonse)
   })
   .catch(error => console.log(error))
-
