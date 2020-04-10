03e3aecc428d90acfd62ede0ef96d6a034567636
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-04-10 12:39:52 -0700

    add a gravity face

--- overlay-underlay/advanced-facial-gravity/sample.js
+++ overlay-underlay/advanced-facial-gravity/sample.js
@@ -5,12 +5,17 @@ const url = cloudinary.url('woman-standing', {
   overlay: 'mask-green',
   gravity: 'adv_face',
   // gravity: 'adv_eyes',
-  // gravity: 'face',
   flags: 'region_relative',
   width: '1.3',
   height: '0.7',
   crop: 'fill',
   y: 30
+  // gravity: 'face',
+  // flags: 'region_relative',
+  // width: '1.1',
+  // crop: 'scale',
+  // x: -10,
+  // y: 50
 })
 console.log(url)
 open(url)
