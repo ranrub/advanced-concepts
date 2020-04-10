a14f57d12ba882766e82b738ab64e982a5d21464
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-04-10 10:21:12 -0700

    fix up facial gravity

--- overlay-underlay/advanced-facial-gravity/sample.js
+++ overlay-underlay/advanced-facial-gravity/sample.js
@@ -1,18 +1,16 @@
 require('dotenv').config()
 const cloudinary = require('cloudinary').v2
 const open = require('open')
-
 const url = cloudinary.url('woman-standing', {
-  crop: 'fill',
-  flags: 'region_relative',
+  overlay: 'mask-green',
   gravity: 'adv_face',
   // gravity: 'adv_eyes',
   // gravity: 'face',
-  y: 30,
-  overlay: 'mask-green',
+  flags: 'region_relative',
   width: '1.3',
-  height: '0.7'
+  height: '0.7',
+  crop: 'fill',
+  y: 30
 })
 console.log(url)
 open(url)
-
